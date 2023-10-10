bee5057d7d92fe78e66a57eb3de03aee6651ca00
Joshua Webb <bbew.auhsoj+git@gmail.com>
2023-10-11 09:08:45 +1100

    Implement final day countdown

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ let clockOffMinutes = 17*60;
 
 function tick() {
   let now = new Date();
-  //now = new Date(2023, 11, 8, 16, 45);
+  //now = new Date(2023, 11, 8, 10, 45);
 
   let diffMs = targetDate - now;
   if (diffMs <= 0) {
@@ -20,17 +20,17 @@ function tick() {
   // TODO: Cache days, only recalc hours / rolling window days if it changes ?
   let workDays = 0;
   let workHours = 0;
+  let workMinutesTotal = 0;
   if (!(now.getDay() == 0 || now.getDay() == 6)) {
     let h = now.getHours();
     let m = h*60 + now.getMinutes();
-    let workMinutes;
     if (m < lunchEndMinutes) {
       // TODO: calc/derive 4.5
-      workMinutes = Math.max(lunchStartMinutes - m, 0) + 4.5*60;
+      workMinutesTotal = Math.max(lunchStartMinutes - m, 0) + 4.5*60;
     } else {
-      workMinutes = clockOffMinutes - m;
+      workMinutesTotal = clockOffMinutes - m;
     }
-    workHours = (workMinutes/60).toFixed(2);
+    workHours = (workMinutesTotal/60).toFixed(2);
   }
 
   let d = new Date(now);
@@ -54,25 +54,42 @@ function tick() {
     container.className = "short";
   }
 
-  if (wholeDays < 1) {
-    // TODO: Hours as major
-  }
-
   let wholeHours = ~~(remainingMs / (1000*60*60));
-  main.innerHTML = `<span class="major">${wholeDays} Days</span>`;
-  if (wholeHours > 0) {
-    let minor = document.createElement("span")
-    minor.class = "minor";
-    minor.textContent =  `${wholeHours} Hours`;
-    main.appendChild(minor);
-  }
+  remainingMs -= wholeHours * 1000*60*60;
+  if (wholeDays < 1) {
+    let wholeMinutes = ~~(remainingMs / (1000*60))
 
-  sub.innerHTML = `<span class="major">${workDays} Days</span>`;
-  if (workHours > 0) {
+    main.innerHTML = `<span class="major">${wholeHours} Hours</span>`;
+    if (wholeMinutes > 0) {
+      let minor = document.createElement("span")
+      minor.class = "minor";
+      minor.textContent =  `${wholeMinutes} Minutes`;
+      main.appendChild(minor);
+    }
+
+    workHours = ~~(workMinutesTotal/60);
+    let workMinutes = workMinutesTotal - workHours*60;
+    sub.innerHTML = `<span class="major">${workHours} Hours</span>`;
     let minor = document.createElement("span")
     minor.class = "minor";
-    minor.textContent =  `${workHours} Hours`;
+    minor.textContent =  `${workMinutes} Minutes`;
     sub.appendChild(minor);
+  } else {
+    main.innerHTML = `<span class="major">${wholeDays} Days</span>`;
+    if (wholeHours > 0) {
+      let minor = document.createElement("span")
+      minor.class = "minor";
+      minor.textContent =  `${wholeHours} Hours`;
+      main.appendChild(minor);
+    }
+
+    sub.innerHTML = `<span class="major">${workDays} Days</span>`;
+    if (workHours > 0) {
+      let minor = document.createElement("span")
+      minor.class = "minor";
+      minor.textContent =  `${workHours} Hours`;
+      sub.appendChild(minor);
+    }
   }
 
   return false;
